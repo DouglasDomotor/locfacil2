61fb2fcf5942fa0df78ae631d94ab64e9b440aa4
Douglas Domotor <dmtr82do@gmail.com>
2025-11-28 19:20:12 -0300

    Add car creation functionality with form and template

--- cars/urls.py
+++ cars/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.car_list, name='car_list'),
+    path('novo/', views.car_create, name='car_create'),
     path('<int:car_id>/', views.car_detail, name='car_detail'),
 ]
--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from.models import Car
+from .forms import CarForm 
 
 def car_list(request):
     cars = Car.objects.all()
@@ -8,3 +9,14 @@ def car_list(request):
 def car_detail(request, car_id):
     car = get_object_or_404(Car, id=car_id)
     return render(request, 'cars/car_detail.html', {'car': car})
+
+def car_create(request):
+    if request.method == 'POST':
+        form = CarForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('car_list')
+    else:
+        form = CarForm()
+
+    return render(request, 'cars/car_create.html', {'form': form})
