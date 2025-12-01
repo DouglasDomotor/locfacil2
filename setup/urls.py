0b98701be7539a59fe687696997afacea365a542
Douglas Domotor <dmtr82do@gmail.com>
2025-12-01 19:48:48 -0300

    Add car detail view and template, update URL routing

--- setup/urls.py
+++ setup/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from cars.views import car_list
+from cars.views import car_list, car_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cars/', include('cars.urls')),
     path('clients/', include('clients.urls')),
     path('cars/', car_list, name="car_list"),
+    path('cars/<int:car_id>/', car_detail, name="car_detail"),
 ]
